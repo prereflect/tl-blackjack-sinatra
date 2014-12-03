3011ff5c10029290466285638861809c673aa8fe
Toby Woller <tobywoller@gmail.com>
2014-12-02 21:23:02 -0800

    Assignment: Set name form
    When your web app first loads (i.e., the '/' route), make sure the
    session has a 'username' set. If not, redirect to a form to set the
    username.

--- main.rb
+++ main.rb
@@ -6,3 +6,15 @@ get '/' do
   erb :index
 end
 
+get '/form' do
+  erb :form
+end
+
+post '/getname' do
+  session[:username] = params[:username]
+  redirect '/bet'
+end
+
+get '/bet' do
+  erb :bet
+end
