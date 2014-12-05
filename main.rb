e4b0242bff24dbb7696aeca98c5ed0ee0f4b11d0
Toby Woller <tobywoller@gmail.com>
2014-12-04 18:34:33 -0800

    Setup session[] Variables, Initial Deal

--- main.rb
+++ main.rb
@@ -2,13 +2,28 @@ require 'sinatra'
 
 use Rack::Session::Cookie, :key => 'rack.session',
                            :path => '/',
-                           :expire_after => 2592000, # In seconds
                            :secret => 'Sj4R#CnTeUOFGQX%XK'
 
 helpers do
+  def new_player
+    session[:player_name] = 'Anon'
+    session[:player_cash] = 500
+    session[:player_hand] = []
+    session[:player_bet] = []
+  end
+
+  def new_deck
+    session[:deck] = [2, 3, 4, 5, 6, 7, 8, 9, 10, 'J', 'Q', 'K', 'A'].product(['C', 'D', 'H', 'S'])
+    session[:deck].shuffle!
+  end
+  
+  def deal(hand)
+    hand << session[:deck].pop
+  end
+
   def link(name)
     case name
-    when :form then '/form'
+    when :name then '/name'
     when :cat then '/cat'
     end
   end
@@ -18,28 +33,37 @@ get '/' do
   erb :index
 end
 
-get '/form' do
-  erb :form
-end
-
 get '/cat' do
   erb :cat
 end
 
-post '/getname' do
-  session[:username] = params[:username]
+get '/name' do
+  erb :name
+end
+
+post '/set_name' do
+  new_player
+  session[:player_name] = params[:player_name]
   redirect '/bet'
 end
 
-post '/bet' do
-  session[:cash] = 500
-  session[:username] = params[:username]
+get '/bet' do
   erb :bet
+end
+
+post '/make_bet' do
+  session[:player_bet] = params[:player_bet].to_i
   redirect '/game'
 end
 
 get '/game' do
-  session[:deck] = [2, 3, 4, 5, 6, 7, 8, 9, 10, 'J', 'Q', 'K', 'A'].product(['C', 'D', 'H', 'S'])
-  session[:deck].shuffle!
+  new_deck
+  session[:dealer_hand] = []
+
+  deal(session[:player_hand])
+  deal(session[:dealer_hand])
+  deal(session[:player_hand])
+  deal(session[:dealer_hand])
+
   erb :game
 end
