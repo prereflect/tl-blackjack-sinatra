99a2d62109355f3170d651a7fb601eee9b3ba864
Toby Woller <tobywoller@gmail>
2014-12-06 03:07:22 -0800

    Misc
    Changes due to change in development environment

--- itp3.sh
+++ itp3.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-cd ~/rails_projects/tl-blackjack-sinatra
+cd ~/Documents/tl-blackjack-sinatra
 
 tmux start-server
 
@@ -7,16 +7,14 @@ tmux new-session -d -s itp3 -n workspace
 tmux new-window -t itp3:2 -n public
 tmux new-window -t itp3:3 -n views
 tmux new-window -t itp3:4 -n irb
-tmux new-window -t itp3:5 -n thin
-tmux new-window -t itp3:6 -n git
+tmux new-window -t itp3:5 git
 
-tmux send-keys -t itp3:2 "cd ~/rails_projects/tl-blackjack-sinatra/public; ls -la" C-m
-tmux send-keys -t itp3:3 "cd ~/rails_projects/tl-blackjack-sinatra/views; ls -la" C-m
+tmux send-keys -t itp3:2 "cd ~/Documents/tl-blackjack-sinatra/public; ls -la" C-m
+tmux send-keys -t itp3:3 "cd ~/Documents/tl-blackjack-sinatra/views; ls -la" C-m
 tmux send-keys -t itp3:4 "irb" C-m
-tmux send-keys -t itp3:5 "shotgun main.rb" C-m
-tmux send-keys -t itp3:6 "git status" C-m
+tmux send-keys -t itp3:5 "git status" C-m
 
-tmux select-window -t itp3:6
+tmux select-window -t itp3:5
 
 tmux attach-session -t itp3
 
