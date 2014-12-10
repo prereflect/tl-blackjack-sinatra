bc328c41bdc7fc02292e2083783f36151039bb73
Toby Woller <tobywoller@gmail.com>
2014-12-09 17:25:20 -0800

    Player Hit
    setup buttons for hit/stay and made /player/hit action

--- main.rb
+++ main.rb
@@ -9,11 +9,10 @@ use Rack::Session::Cookie, :key => 'rack.session',
  DEALER_STAY = 17
 
 helpers do
-  def new_player
-    session[:player_name] = ''
+  def player
     session[:player_cash] = 500
     session[:player_hand] = []
-    session[:player_bet] = []
+    session[:player_bet] = ''
   end
 
   def new_deck
@@ -52,16 +51,22 @@ get '/' do
   erb :index
 end
 
-get '/cat' do
-  erb :cat
+get '/new_game' do
+  new_deck
+  player
+  session[:dealer_hand] = []
+  2.times do
+    deal(session[:player_hand])
+    deal(session[:dealer_hand])
+  end
+  redirect '/name'
 end
 
 get '/name' do
   erb :name
 end
 
-post '/set_name' do
-  new_player
+post '/post_name' do
   session[:player_name] = params[:player_name]
   redirect '/bet'
 end
@@ -70,27 +75,21 @@ get '/bet' do
   erb :bet
 end
 
-post '/make_bet' do
+post '/post_bet' do
   session[:player_bet] = params[:player_bet].to_i
   redirect '/game'
 end
 
+get '/player/hit' do
+  deal(session[:player_hand])
+  redirect '/game'
+end
+
 get '/game' do
-  new_deck
-  session[:dealer_hand] = []
-
-  2.times do
-    deal(session[:player_hand])
-    deal(session[:dealer_hand])
-  end
-
   erb :game
 end
 
-get '/new_game' do
-  new_deck
-  session[:player_cash] = 500
-  session[:player_hand] = []
-  session[:player_bet] = []
-  redirect '/bet'
+get '/cat' do
+  erb :cat
 end
+
