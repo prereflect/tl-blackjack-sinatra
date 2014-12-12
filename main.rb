8edc5c70604f2128086710ae15870666de94a097
Toby Woller <tobywoller@gmail.com>
2014-12-12 02:54:55 -0800

    Change CSS color background-color

--- main.rb
+++ main.rb
@@ -17,7 +17,7 @@ helpers do
 
   def new_deck
     session[:deck] = [
-      2, 3, 4, 5, 6, 7, 8, 9, 10, 'J', 'Q', 'K', 'A'].product(
+      2, 3, 4, 5, 6, 7, 8, 9, 10, 'jack', 'queen', 'king', 'ace'].product(
       ['C', 'D', 'H', 'S'])
     session[:deck].shuffle!
   end
